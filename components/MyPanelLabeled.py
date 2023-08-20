d10e59d07d3bb9103329e5b2b3056e3ff91260ef
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-20 13:40:17 +0200

    Custom GroupBox

---
+++ components/MyPanelLabeled.py
@@ -0,0 +1,24 @@
+from PyQt5 import QtWidgets
+
+from components.MyWidgetWithLayout import MyWidgetWithLayout
+
+
+class MyPanelLabeled(QtWidgets.QGroupBox, MyWidgetWithLayout):
+    def __init__(self, name=None, layout=None, label: str = None):
+        super(MyPanelLabeled, self).__init__()
+
+        # Name of the component
+        if name is not None:
+            self.setObjectName(name)
+
+        # Layout of the component
+        if layout is None:
+            self.setLayout(QtWidgets.QVBoxLayout())
+        else:
+            self.setLayout(layout)
+        self.removeLayoutMargins()
+
+        # Label (text) of the component
+        if label is not None:
+            self.setTitle(label)
+
