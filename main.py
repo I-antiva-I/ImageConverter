43085c5003cbcaaa4b002cdb745be74e40fa9467
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-21 16:53:01 +0200

    mainWindowController added

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import time
 from PyQt5 import QtWidgets
 
 from components.MyMainWindow import MyMainWindow
+from controllers.MainWindowController import MainWindowController
 
 
 def launchApp():
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = MyMainWindow()
 
-    # mainWindowController = MainWindowController(mainWindow)
-    # mainWindowController.connectFunctions()
+    mainWindowController = MainWindowController(mainWindow.components)
+    mainWindowController.setup()
+    mainWindowController.connectFunctions()
 
     mainWindow.show()
     sys.exit(app.exec_())
