086a77f80e5c6df373dddb0341b9c18976f0402d
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-21 16:54:38 +0200

    StatusLabelColor added

--- utility/enums.py
+++ utility/enums.py
@@ -4,3 +4,12 @@ from enum import Enum
 class DialogMode(Enum):
     DIRECTORY = 0
     FILE = 1
+
+
+class StatusLabelColor(Enum):
+    DEFAULT =       "#FFFFFF"       # white
+    INFORMATION =   "#1E90EE"       # blue
+    SUCCESS =       "#3CB371"       # green
+    WARNING =       "#EEC700"       # yellow
+    ERROR =         "#EE0000"       # red
+
