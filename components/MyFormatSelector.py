462726d7f06d6d2c28c349537a3b2a11837aaf5e
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-21 08:17:56 +0200

    Functions for content manipulation

--- components/MyFormatSelector.py
+++ components/MyFormatSelector.py
@@ -1,8 +1,8 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QSizePolicy
 
 from .MyLabel import MyLabel
 from .MyPanel import MyPanel
-from .MyWidgetWithLayout import MyWidgetWithLayout
 
 
 class MyFormatSelector(MyPanel):
@@ -10,29 +10,58 @@ class MyFormatSelector(MyPanel):
         super(MyFormatSelector, self).__init__(name, QtWidgets.QGridLayout())
 
         # Combo boxes for formats
-        comboBoxOriginal = QtWidgets.QComboBox()
-        comboBoxTarget = QtWidgets.QComboBox()
+        self.comboBoxOriginal = QtWidgets.QComboBox()
+        self.comboBoxTarget = QtWidgets.QComboBox()
+
+        # Size policies
+        self.comboBoxOriginal.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        self.comboBoxTarget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+
+        # Connect onChanged for comboBoxOriginal
+        self.comboBoxOriginal.currentIndexChanged.connect(self.onComboBoxOriginalChanged)
 
         # Labels for formats
         labelOriginal = MyLabel("Original format")
         labelTarget = MyLabel("Target format")
 
-        # Combo boxes items TEMP
-        comboBoxOriginal.addItem("Alpha")
-        comboBoxOriginal.addItem("Beta")
-        comboBoxOriginal.addItem("Gamma")
-        comboBoxOriginal.addItem("Delta")
-        comboBoxOriginal.addItem("Epsilon")
-        comboBoxTarget.addItem("Zetta")
-        comboBoxTarget.addItem("Omega")
-        comboBoxTarget.addItem("Lambda")
-        comboBoxTarget.addItem("Theta")
-        comboBoxTarget.addItem("Phi")
+        # Available formats (keys for comboBoxOriginal, values for comboBoxTarget)
+        self.availableFormats = {}
 
-        # Placement
-        self.place(labelOriginal,       1, 1, 1, 2)
-        self.place(comboBoxOriginal,    1, 3, 1, 1)
-        self.place(labelTarget,         2, 1, 1, 2)
-        self.place(comboBoxTarget,      2, 3, 1, 1)
+        # Placement of components
+        self.place(labelOriginal,               1, 1, 1, 2)
+        self.place(self.comboBoxOriginal,       1, 3, 1, 1)
+        self.place(labelTarget,                 2, 1, 1, 2)
+        self.place(self.comboBoxTarget,         2, 3, 1, 1)
+
+        # Default state
+        self.comboBoxTarget.setEnabled(False)
+
+    # ComboBoxOriginal functions
+    def clearComboBoxOriginal(self):
+        self.comboBoxOriginal.clear()
+
+    def setStateComboBoxOriginal(self, state: bool):
+        self.comboBoxOriginal.setEnabled(state)
+
+    def fillComboBoxOriginal(self, items):
+        self.comboBoxOriginal.addItems(items)
+
+    def onComboBoxOriginalChanged(self):
+        self.clearComboBoxTarget()
+        self.fillComboBoxTarget(self.availableFormats[self.comboBoxOriginal.currentText()])
+
+    # ComboBoxTarget functions
+    def clearComboBoxTarget(self):
+        self.comboBoxTarget.clear()
+
+    def setStateComboBoxTarget(self, state: bool):
+        self.comboBoxTarget.setEnabled(state)
+
+    def fillComboBoxTarget(self, items):
+        self.comboBoxTarget.addItems(items)
+
+
+
+
 
 
--- components/MyPathSelector.py
+++ components/MyPathSelector.py
@@ -1,21 +1,82 @@
+import pyperclip
+
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QTextEdit, QLineEdit
+from PyQt5.QtWidgets import QTextEdit, QLineEdit, QFileDialog
 
 from .MyPanel import MyPanel
 from .MyPushButton import MyPushButton
-from .MyWidgetWithLayout import MyWidgetWithLayout
+from utility.enums import DialogMode
 
 
 class MyPathSelector(MyPanel):
-    def __init__(self, name=None, layout=None):
-        super(MyPathSelector, self).__init__(name, layout)
+    def __init__(self, name=None, dialogMode=DialogMode.FILE):
+        super(MyPathSelector, self).__init__(name, layout=QtWidgets.QHBoxLayout())
 
         # File dialog button
+        self.dialogMode = dialogMode
         self.dialogButton = MyPushButton(text="Path")
+        self.dialogButton.clicked.connect(self.onDialogButtonClicked)
+        self.nameFilterOptions = ""
+
+        # Controls for line edit
+        clearButton = MyPushButton(text="DELETE")
+        pasteButton = MyPushButton(text="PASTE")
+        clearButton.clicked.connect(self.onClearButtonClicked)
+        pasteButton.clicked.connect(self.onPasteButtonClicked)
 
         # Path line edit
         self.pathLineEdit = QLineEdit()
+        self.pathLineEdit.textChanged.connect(self.onPathEditLineChanged)
         self.pathLineEdit.setSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Minimum)
 
         # Placement
-        self.placeAll(self.dialogButton, self.pathLineEdit)
+        self.placeAll(self.dialogButton, self.pathLineEdit, clearButton, pasteButton)
+
+    def resetNameFilterOptions(self, label, extensions):
+        if extensions is None:
+            self.nameFilterOptions = "Any (*);;"
+        else:
+            options = f"{label} ("
+            for extension in extensions:
+                options += "*"+extension+" "
+            options += ");;"
+
+            self.nameFilterOptions = options
+
+    def onClearButtonClicked(self):
+        self.pathLineEdit.setText("")
+
+    def onPasteButtonClicked(self):
+        text = pyperclip.paste()
+        if text != "":
+            self.pathLineEdit.setText(text)
+        else:
+            self.pathLineEdit.setText("?")
+
+    def onPathEditLineChanged(self):
+        print(">", self.pathLineEdit.displayText())
+
+    def onDialogButtonClicked(self):
+        # Create file dialog
+        dialog = QFileDialog(self)
+
+        # Select dialog mode (directory/file)
+        if self.dialogMode == DialogMode.DIRECTORY:
+            dialog.setFileMode(QFileDialog.FileMode.DirectoryOnly)
+        elif self.dialogMode == DialogMode.FILE:
+            dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+            dialog.setNameFilter(self.nameFilterOptions+"All (*)")
+        else:
+            return
+
+        # More detailed view
+        dialog.setViewMode(QFileDialog.ViewMode.Detail)
+
+        dialogPath = ""
+        # Open dialog window and select path
+        if dialog.exec():
+            dialogPath = dialog.selectedFiles()[0]
+
+        print(dialogPath)
+        self.pathLineEdit.setText(dialogPath)
+
