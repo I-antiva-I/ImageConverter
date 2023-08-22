9cc4323a3ff0ec3345b7263dd55211d999665801
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-22 19:50:13 +0200

    Setters for text and color

--- components/MyLabel.py
+++ components/MyLabel.py
@@ -4,13 +4,24 @@ from components.MyWidgetWithLayout import MyWidgetWithLayout
 
 
 class MyLabel(QtWidgets.QLabel):
-    def __init__(self, text="Placeholder text", name=None):
+    def __init__(self, text="Placeholder text", name=None, color=None):
         super(MyLabel, self).__init__()
 
         # Widget name
         if name is not None:
             self.setObjectName(name)
 
-        # Text
+        # Label text
         self.setText(text)
+
+        # Text color
+        if color is not None:
+            self.setStyleSheet(f"color: {color};")
+
+    def changeText(self, text):
+        self.setText(text)
+
+    def changeColor(self, color):
+        self.setStyleSheet(f"color: {color};")
+
 
